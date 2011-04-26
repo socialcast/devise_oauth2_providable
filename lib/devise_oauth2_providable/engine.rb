ea3f549849b1c1ea21a2bd76d66c4c725bd00011
Ryan Sonnek <ryan@socialcast.com>
2011-04-26 17:33:27 -0500

    filter client secret out of logs

--- lib/devise_oauth2_providable/engine.rb
+++ lib/devise_oauth2_providable/engine.rb
@@ -2,6 +2,7 @@ module Devise
   module Oauth2Providable
     class Engine < Rails::Engine
       initializer "devise_oauth2_providable.initialize_application" do |app|
+        app.config.filter_parameters << :client_secret
         app.middleware.use Rack::OAuth2::Server::Resource::Bearer, 'OAuth2 Bearer Token Resources' do |req|
           AccessToken.valid.find_by_token(req.access_token) || req.invalid_token!
         end
