f8958662183ed802d01afe09dce5861ea737fa4d
Ryan Sonnek <ryan@socialcast.com>
2011-05-11 16:23:36 -0500

    update tests

--- spec/rails_app/spec/models/client_spec.rb
+++ spec/rails_app/spec/models/client_spec.rb
@@ -1,3 +1,5 @@
+require 'spec_helper'
+
 describe Client do
   describe 'basic client instance' do
     before { @client = Client.create :name => 'test', :redirect_uri => 'http://localhost:3000' }
--- spec/rails_app/spec/models/user_spec.rb
+++ spec/rails_app/spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'basic user instance' do
+    before { @user = User.create :name => 'ryan sonnek' }
+    it { should have_many :access_tokens }
+  end
 end
