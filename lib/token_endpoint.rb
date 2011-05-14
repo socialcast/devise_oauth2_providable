8381fd55553751d71188f5da51ff73107c5834b7
Ryan Sonnek <ryan@socialcast.com>
2011-05-14 00:27:18 -0500

    use exceptions to control flow

--- lib/token_endpoint.rb
+++ lib/token_endpoint.rb
@@ -1,5 +1,5 @@
 class TokenEndpoint
-
+  class InvalidGrantType < StandardError; end
   def call(env)
     authenticator.call(env)
   end
@@ -11,10 +11,12 @@ class TokenEndpoint
       client = Client.find_by_identifier(req.client_id) || req.invalid_client!
       client.secret == req.client_secret || req.invalid_client!
 
-      refresh_token = refresh_token(req, client)
-      if refresh_token && token = refresh_token.access_tokens.create(:client => client, :user => refresh_token.user)
-        res.access_token = token.to_bearer_token
-      else
+      begin
+        refresh_token = find_refresh_token(req, client)
+        access_token = refresh_token.access_tokens.create!(:client => client, :user => refresh_token.user)
+        res.access_token = access_token.to_bearer_token
+      rescue => e
+        puts e.inspect
         req.invalid_grant!
       end
     end
@@ -22,24 +24,25 @@ class TokenEndpoint
 
   # NOTE: extended assertion grant_types are not supported yet.
   # NOTE: client_credentials grant_types are not yet supported
-  def refresh_token(req, client)
+  def find_refresh_token(req, client)
     case req.grant_type
     when :authorization_code
       code = AuthorizationCode.valid.find_by_token(req.code)
-      return nil unless code.valid_request?(req)
+      raise InvalidGrantType.new('invalid authorization code') unless code && code.valid_request?(req)
       client.refresh_tokens.create! :user => code.user
     when :password
       resource = mapping.to.find_for_authentication(mapping.to.authentication_keys.first => req.username)
-      return nil unless resource && resource.respond_to?(:valid_password?)
+      raise InvalidGrantType.new('user not found') unless resource
+      raise InvalidGrantType.new('user does not support password authentication') unless resource.respond_to?(:valid_password?)
       valid = resource.valid_for_authentication? { resource.valid_password?(req.password) }
-      return nil unless valid.is_a?(TrueClass)
+      raise InvalidGrantType.new("authentication failed: #{valid}") unless valid.is_a?(TrueClass)
       client.refresh_tokens.create! :user => resource
     when :refresh_token
       refresh_token = client.refresh_tokens.valid.find_by_token(req.refresh_token)
-      return nil unless refresh_token.present?
+      raise InvalidGrantType.new('refresh token not found') unless refresh_token
       refresh_token
     else
-      nil
+      raise InvalidGrantType.new('invalid grant type')
     end
   end
   def mapping
