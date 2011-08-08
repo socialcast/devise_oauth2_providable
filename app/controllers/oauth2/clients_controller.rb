099c2777f9d164c7fd7392745ae06cefd6762061
Velimir Lesikov <velimirles@gmail.com>
2011-08-08 13:42:36 -0700

    Adding Client CRUD New client can be created using the REST URLs - oauth2/clients

---
+++ app/controllers/oauth2/clients_controller.rb
@@ -0,0 +1,85 @@
+class Oauth2::ClientsController < ApplicationController
+  before_filter :authenticate_user!
+  
+  # GET /clients
+  # GET /clients.xml
+  def index
+    @clients = Client.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @clients }
+    end
+  end
+
+  # GET /clients/1
+  # GET /clients/1.xml
+  def show
+    @client = Client.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @client }
+    end
+  end
+
+  # GET /clients/new
+  # GET /clients/new.xml
+  def new
+    @client = Client.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @client }
+    end
+  end
+
+  # GET /clients/1/edit
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  # POST /clients
+  # POST /clients.xml
+  def create
+    @client = Client.new(params[:client])
+
+    respond_to do |format|
+      if @client.save
+        format.html { redirect_to( oauth2_clients_path, :notice => 'Client was successfully created.') }
+        format.xml  { render :xml => @client, :status => :created, :location => @client }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @client.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /clients/1
+  # PUT /clients/1.xml
+  def update
+    @client = Client.find(params[:id])
+
+    respond_to do |format|
+      if @client.update_attributes(params[:client])
+        format.html { redirect_to( oauth2_clients_path, :notice => 'Client was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @client.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /clients/1
+  # DELETE /clients/1.xml
+  def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(oauth2_clients_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
