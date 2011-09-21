3db0d10e8b42dd7f5e3823fc6df22feab2529609
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2011-09-21 02:35:11 -0400

    add missing

---
+++ _active_record_models/authorization_code.rb
@@ -0,0 +1,11 @@
+require 'expirable_token'
+
+class AuthorizationCode < ActiveRecord::Base
+  include ExpirableToken
+  def access_token
+    @access_token ||= expired! && user.access_tokens.create(:client => client)
+  end
+  def valid_request?(req)
+    self.redirect_uri == req.redirect_uri
+  end
+end
