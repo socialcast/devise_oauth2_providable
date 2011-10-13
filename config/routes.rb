8cd41db19366a2332889dcdb64d5034c9cd7c4d5
Ryan Sonnek <ryan@socialcast.com>
2011-10-13 17:13:33 -0500

    update files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Devise::Oauth2Providable::Engine.routes.draw do
-  resources :authorizations, :only => :create
-  resource :token, :only => :create
+  root :to => "authorizations#new"
 
-  # scope '/oauth2', :as => 'oauth2' do
-  #   resources :authorizations, :controller => 'oauth2/authorizations', :only => :create
-  #   match 'authorize' => 'oauth2/authorizations#new'
-  #   resource :token, :controller => 'oauth2/tokens', :only => :create
-  # end
+  resources :authorizations, :only => :create
+  match 'authorize' => 'authorizations#new'
+  resource :token, :only => :create
 end
