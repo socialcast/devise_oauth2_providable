dcf0735709cb1c24fd3c78e27e46e569e9524c5d
Ryan Sonnek <ryan@socialcast.com>
2011-10-31 09:40:16 -0500

    add rails route loader for tests

--- spec/routing/authorizations_routing_spec.rb
+++ spec/routing/authorizations_routing_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
 describe Devise::Oauth2Providable::AuthorizationsController do
+  before :all do
+    Devise::Oauth2Providable::Engine.load_engine_routes
+  end
   describe 'routing' do
     it 'routes POST /oauth2/authorizations' do
-      {:post => '/oauth2/authorizations'}.should route_to(:controller => 'oauth2/authorizations', :action => 'create')
+      post('/oauth2/authorizations').should route_to('devise/oauth2_providable/authorizations#create')
     end
     it 'routes GET /oauth2/authorize' do
-      {:get => '/oauth2/authorize'}.should route_to(:controller => 'oauth2/authorizations', :action => 'new')
+      get('/oauth2/authorize').should route_to('devise/oauth2_providable/authorizations#new')
     end
     it 'routes POST /oauth2/authorize' do
-      {:post => '/oauth2/authorize'}.should route_to(:controller => 'oauth2/authorizations', :action => 'new')
+      #FIXME: this is valid, but the route is not being loaded into the test
+      post('/oauth2/authorize').should route_to('devise/oauth2_providable/authorizations#new')
     end
   end
 end
