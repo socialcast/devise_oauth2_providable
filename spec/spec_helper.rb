00c78b7a8ec05ddbe29eb9344455cd68a290185d
Konstantin Tennhard <me@t6d.de>
2011-11-15 16:13:34 +0100

    Improves the spec helper a lot - Auto Migration before testing, improved code readability

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,29 +1,45 @@
+##
 # Configure Rails Envinronment
+##
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require 'rails/test_help'
 
-require 'pry'
+##
+# Setup RSpec
+##
+
 require 'rspec/rails'
-require 'shoulda-matchers'
-
-require 'factory_girl_rspec'
-FactoryGirl.definition_file_paths = [
-    File.join(File.dirname(__FILE__), 'factories')
-]
-FactoryGirl.find_definitions
-
-ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
-
 RSpec.configure do |config|
   config.include Devise::TestHelpers, :type => :controller
-
   config.use_transactional_fixtures = true
 
-  # enable rendering of views for controller tests
-  # see http://stackoverflow.com/questions/4401539/rspec-2-how-to-render-views-by-default-for-all-controller-specs
+  # Enable rendering of views for controller tests
+  # See SOF question: http://bit.ly/sof-rspec-controller-view-rendering
   config.render_views
 end
+
+spec_root = File.expand_path('..', __FILE__)
+
+##
+# Setup FactoryGirl
+## 
+
+require 'factory_girl_rspec'
+FactoryGirl.definition_file_paths = [File.join(spec_root, 'factories')]
+FactoryGirl.find_definitions
+
+require 'shoulda-matchers'
+
+##
+# Run Migrations
+##
+
+ActiveRecord::Migrator.migrate(File.expand_path("dummy/db/migrate/", spec_root))
+
+##
+# Load supporting files
+##
+
+Dir["#{spec_root}/support/**/*.rb"].each { |f| require f }
