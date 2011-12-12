2999df8c8dc80c8f91409c001c9ae67fe28bc321
Matt Button <matthew@sigswitch.com>
2011-12-12 23:20:37 +0000

    Use models defined by mappings in auth. controller, refs #23

--- app/controllers/devise/oauth2_providable/authorizations_controller.rb
+++ app/controllers/devise/oauth2_providable/authorizations_controller.rb
@@ -34,17 +34,19 @@ module Devise
       end
 
       def authorize_endpoint(allow_approval = false)
+        models = self.devise_oauth_mapping.models
+
         Rack::OAuth2::Server::Authorize.new do |req, res|
-          @client = Client.find_by_identifier(req.client_id) || req.bad_request!
+          @client = models[:client].find_by_identifier(req.client_id) || req.bad_request!
           res.redirect_uri = req.verify_redirect_uri!(@client.redirect_uri)
           if allow_approval
             if params[:approve].present?
               case req.response_type
               when :code
-                authorization_code = current_user.authorization_codes.create!(:client => @client)
+                authorization_code = self.resource.authorization_codes.create!(:client => @client)
                 res.code = authorization_code.token
               when :token
-                access_token = current_user.access_tokens.create!(:client => @client).token
+                access_token = self.resource.access_tokens.create!(:client => @client).token
                 bearer_token = Rack::OAuth2::AccessToken::Bearer.new(:access_token => access_token)
                 res.access_token = bearer_token
                 res.uid = resource.id
--- lib/devise/oauth2_providable/controllers/helpers.rb
+++ lib/devise/oauth2_providable/controllers/helpers.rb
@@ -4,12 +4,15 @@ module Devise
     module Controllers
       module Helpers
 
-				# Authenticates the current scope and gets the current resource from the session.
-				# Taken from devise
-				def authenticate_scope!
-					send(:"authenticate_#{resource_name}!", :force => true)
-					self.resource = send(:"current_#{resource_name}")
-				end
+        # Authenticates the current scope and gets the current resource from the session.
+        # Taken from devise
+        def authenticate_scope!
+          send(:"authenticate_#{resource_name}!", :force => true)
+          self.resource = send(:"current_#{resource_name}")
+        end
+        def devise_oauth_mapping
+          @devise_oauth_mapping ||= request.env[Devise::Oauth2Providable::MAPPING_ENV_REF]
+        end
       end
     end
   end
