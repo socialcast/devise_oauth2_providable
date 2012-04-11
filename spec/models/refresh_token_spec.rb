ff51e0ad6e2880657278d2d125d2dfa3f75f7e0c
Karl Freeman <karlfreeman@gmail.com>
2012-04-11 17:47:18 +0100

    add tests

--- spec/models/refresh_token_spec.rb
+++ spec/models/refresh_token_spec.rb
@@ -11,7 +11,9 @@ describe Devise::Oauth2Providable::RefreshToken do
     it { should validate_presence_of :token }
     it { should validate_uniqueness_of :token }
     it { should belong_to :user }
+    it { should allow_mass_assignment_of :user }
     it { should belong_to :client }
+    it { should allow_mass_assignment_of :client }
     it { should validate_presence_of :client }
     it { should validate_presence_of :expires_at }
     it { should have_many :access_tokens }
